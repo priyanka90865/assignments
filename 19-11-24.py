1efc3ab6acb5ceeb93cc99f5830c08b81ed34332
priyanka <priyamkap90865@gmail.com>
2024-11-19 21:08:30 +0530

    intial commit

---
+++ 19-11-24.py
@@ -0,0 +1,49 @@
+#control flow statements
+#if ,elif ,else #conditional statement
+"""
+if >>> executes block of code when condition is True
+
+elif >>>> test another condition when if condition is failed (used for having multiple if condition)
+
+else >>>> executes whenever if and elif is failed
+"""
+
+"""x=5
+if x==5:
+    print("x is 5")
+elif x<5:
+    print("x is less than 5")
+else:
+    print("x is greater than 5")"""
+
+x=20
+y=30
+
+if x==y:
+    print("x and y are equal values")
+   
+else:
+    print("x and y are not equal")
+
+age=18
+x=int(input("enter your age :"))
+
+if x>=18 :
+    print("your eligible to vote")
+else :
+    print("your not eligible to vote")
+
+print("..............")
+
+if x > 18:
+    print("your eligible for vote")
+
+elif x==18:
+    print("your eligible to vote")
+
+else :
+    print("your not eligile to vote")
+
+
+
+
