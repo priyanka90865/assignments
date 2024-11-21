245ca0b55dd406285717e777d497a45349c3b0ad
priyanka <priyamkap90865@gmail.com>
2024-11-21 19:15:11 +0530

    Daily task

---
+++ 20-11-2024.py
@@ -0,0 +1,73 @@
+"""GIT:
+
+Git is a free, open-source version control system (VCS) that's used to track code changes and maintain a history of modifications.
+It's the most widely used VCS in the world and is considered the modern standard for software development. 
+Here are some features of Git.
+
+• Distributed: Each developer has a local clone of the project's history, so they can work offline or remotely. 
+• Branching: Developers can create a copy of their source code, called a branch, to work on in parallel to the main version. 
+• Merging: Developers can merge their branch back into the main version.
+• Committing: Developers can commit their work locally, and then sync their copy of the repository with the copy on the server. 
+• Reverting: Developers can revert to older versions of the code and undo changes.
+• Pull requests: A popular tool that allows teams to collaborate on Git branches and review each other's code.
+
+GITHUB:
+
+GitHub is a web-based platform that allows developers
+
+Store, share, and collaborate on code
+Users can create, store, and manage their code in a "repository". 
+They can also share their work, track changes, and collaborate on projects with others.
+ 
+Use version control
+GitHub uses Git, an open source version control system, to track changes to code and reduce duplicating work.
+ 
+Use management tools
+GitHub provides tools for managing changes from multiple developers, including pull requests, which show differences in colors.
+ 
+Choose repository visibility
+Users can choose to make their repositories public or private. 
+Public repositories are accessible to everyone on the internet, while private repositories are only accessible to the owner and people they share access with. 
+GitHub is popular because it's: Easy to use, Has robust documentation and support, and Encourages collaboration. 
+GitHub was founded in 2008 by Tom Preston-Werner, Chris Wanstrath, P. J. Hyett, and Scott Chacon. In 2018, Microsoft acquired GitHub for $7.5 billion. 
+
+VERSION CONTROL SYSTEM:
+
+A version control system (VCS) tracks changes to files and allows developers to collaborate and return to earlier versions.
+VCSs are also known as source control or revision control.
+
+Here are some types of VCSs: 
+Centralized version control systems (CVCS)
+A single server stores all file versions, backup files, and the number of contributors.
+Developers borrow files to edit and return them with updates.
+ 
+Distributed version control systems (DVCS)
+Each developer has the entire project history locally on their computer.
+This allows for offline work and branching and merging strategies.
+ 
+Local version control system
+A local database on a computer stores file changes as patches. 
+To recreate a file, all patches are added up. 
+
+Mercurial
+A VCS designed to handle large distributed projects.
+ 
+Git
+A VCS that tracks changes to a codebase. 
+
+Subversion
+A VCS that tracks changes to files, folders, and directories. 
+This helps with data recovery and provides a history of changes.
+ 
+Concurrent versions system (CVS)
+Multiple developers can work on a project concurrently. 
+Each developer has a copy of the source tree from a central repository.
+ 
+Unity Version Control
+A VCS used to track changes to the source code of mobile games. 
+It helps maintain coordination among the team.
+ 
+AWS CodeCommit
+A managed VCS that hosts private Git repositories in secure AWS environments"""
+
+
