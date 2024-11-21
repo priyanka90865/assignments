af5fc7fd84f42b852f1248ff7af6fc0fa7776a80
priyanka <priyamkap90865@gmail.com>
2024-11-21 18:24:23 +0530

    task is added

---
+++ 21-11-24.py
@@ -0,0 +1,62 @@
+2
+print(str3)
+
+# write a porgram to append a string using assignment oprerators.
+srt1 = "hello"
+name = input(  "\n Enter your name: ")
+str1 += " "+ name
+str1 += " "+ "welcome to python programming"
+print(str1)
+
+print('---------------------------------------------------------------------')
+
+# string slicing :-
+
+x= "welcome to India"
+print(len(x))
+print(x[13])
+print(x[-10])
+print(x[0:10])
+
+print(x[-5:-11])
+
+print(x[-11:-5])
+
+print(x[10:5])
+
+print(x[5:10])
+
+print(x[1:10:2])
+
+print(x[-15:-1:-3])
+
+
+# string methods:-
+
+x= input("Enter a word or sentence: ")
+print("uppercase - ", x.upper())
+
+print("lowercase - ", x.lower())
+
+print("title - ", x.title())
+
+print("count - ", x.count("p"))
+
+print("split - ", x.split())
+print(type(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
