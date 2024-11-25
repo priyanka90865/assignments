ae8a5603215e361ee0eef68c057e69a4a462e0ad
priyanka <priyamkap90865@gmail.com>
2024-11-25 19:29:30 +0530

    Daily task

---
+++ 22-11-2024.py
@@ -0,0 +1,15 @@
+#strings >>>> collection of characters represent in between '',"",""" """
+name = "sai"
+print(name)
+a = 'animal'
+print(type(name))
+print(type(a))
+abc = """ hello world 
+python is a very easy language"""
+print(abc)
+print(type(abc))
+
+#list >>>> collection of all the data types and store in a single variable 
+details = ["abhay",88,55.55,True]
+print(details)
+print(type(details))
