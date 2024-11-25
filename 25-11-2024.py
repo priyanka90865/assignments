613ac5f3dd64d4300ef8c105b98f24304108f5d2
priyanka <priyamkap90865@gmail.com>
2024-11-25 19:08:59 +0530

    Daily task

---
+++ 25-11-2024.py
@@ -0,0 +1,26 @@
+#finding of substring in the main string
+
+#find() returns index position on substring in main string
+
+s="welcome to world of python"
+
+print(s.find("world"))
+
+print(s.find("zebra"))
+
+
+#index() returns index position on substring in main string
+
+s="welcome to world of python"
+
+
+print(s.index("world"))
+
+print(s.find("w"))
+
+print(s.rfind("w"))
+
+print(s.rindex('o'))
+
+print(s.index("o"))
+
