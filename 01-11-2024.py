9aed35368cb9570c5784a2ebe634b3a5c27ea954
priyanka <priyamkap90865@gmail.com>
2024-11-27 20:45:20 +0530

    Daily task

---
+++ 01-11-2024.py
@@ -0,0 +1,9 @@
+#Python program to convert lowercase char to uppercase of string. 
+
+s=input("Enter a string: ")
+print(s.upper())
+
+#Python program to check given character is digit or not using isdigit() method. 
+
+sd=input("Enter a string: ")
+print(sd.isdigit())
---
+++ 04-11-2024.py
@@ -0,0 +1,10 @@
+# Python program to separate characters in a given string. 
+ss=input("Enter a string: ")
+print(ss.split())
+
+# Python program to remove blank space from string. 
+ss1=input("Enter a string: ")
+print(len(ss1))
+ss2=(ss1.strip())
+print(len(ss2))
+print(ss2)
